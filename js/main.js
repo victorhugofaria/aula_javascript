19353ba475fadb4ab3a0b80d36328028e4a4c2ef
Victor Hugo Faria da Silva <64808653+victorhugofaria@users.noreply.github.com>
2021-08-26 10:55:44 -0400

    aula1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let nome = "Victor Hugo";
+let idade = 34;
+let idade2 = 10;
+let frase = "Japão é o melhor time do mundo";
+
+//alert(nome + " tem " + idade + " anos ");
+
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
